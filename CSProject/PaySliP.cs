cd56267d06301aa95e1dc11afe07a11ae0ea1abd
Abram Murphy <abemurf@gmail.com>
2020-12-18 08:02:44 -0500

    First commit

---
+++ CSProject/PaySliP.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using CSProject;
+
+class PaySliP
+{
+    private int month;
+    private int year;
+    private int payMonth;
+    private int payYear;
+
+    enum MonthsOfYear { JAN = 1, FEB = 2, MAR = 3, APR = 4, MAY = 5, JUN = 6, JUL = 7, AUG = 8, SEP = 9, OCT = 10, NOV = 11, DEC = 12 }
+
+    public void GeneratePaySlip(List <Staff> myStaff)
+    {
+        month = payMonth;
+        year = payYear;
+    }  
+}
